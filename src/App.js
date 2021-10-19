65371680058d1338e6806c9e73303c778b6f15cd
Joe Zucco <josephzucco@gmail.com>
2021-10-18 20:22:25 -0400

    testing checkbox

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
@@ -5,13 +7,25 @@ import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 function CheckboxExample() {
+  const [checked, setChecked] = useState(true);
   return (
-    
-  )
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          inputProps={{
+            "aria-label": "secondary checkbox",
+          }}
+        />
+      }
+      label='Testing Checkbox'
+    />
+  );
 }
-
 
 function App() {
   return (
