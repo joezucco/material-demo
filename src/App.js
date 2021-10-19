e3e3fac74e45aa18499f77c6bffba2e67bf69633
Joe Zucco <josephzucco@gmail.com>
2021-10-18 20:14:58 -0400

    create checkbox example functional component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+
+function CheckboxExample() {
+  return (
+    
+  )
+}
+
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <CheckboxExample />
         <ButtonGroup variant='contained' color='primary'>
           <Button startIcon={<SaveIcon />}>Save</Button>
           <Button startIcon={<DeleteIcon />}>Discard</Button>
