589693f8d73bfaf6bd3a4bbcab811a70ae476c7c
Joe Zucco <josephzucco@gmail.com>
2021-10-18 20:07:02 -0400

    import icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Button
+          startIcon={<SaveIcon />}
           size='large'
           variant='contained'
           color='secondary'
