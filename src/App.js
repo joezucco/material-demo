e280f1d687de8dc1f09daa7fb5a54523e01932ea
Joe Zucco <josephzucco@gmail.com>
2021-10-18 20:12:42 -0400

    import button group from material-ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Button
-          startIcon={<SaveIcon />}
-          size='large'
-          variant='contained'
-          color='secondary'
-        >
-          Hello World
-        </Button>
+        <ButtonGroup variant='contained' color='primary'>
+          <Button startIcon={<SaveIcon />}>Save</Button>
+          <Button startIcon={<DeleteIcon />}>Discard</Button>
+        </ButtonGroup>
         <img src={logo} className='App-logo' alt='logo' />
       </header>
     </div>
