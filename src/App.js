45bf57156304ae88bd65d46086d8fe6e3febc803
Joe Zucco <josephzucco@gmail.com>
2021-10-19 12:37:13 -0400

    update

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function CheckboxExample() {
       control={
         <Checkbox
           checked={checked}
+          icon={<DeleteIcon />}
+          checkedIcon={<SaveIcon />}
           onChange={(e) => setChecked(e.target.checked)}
           inputProps={{
             "aria-label": "secondary checkbox",
